557c2bf7bd8d670d33287058b57c256739df7114
Timur Rezaei <rezai.timur.04@gmail.com>
2024-05-10 14:24:44 -0700

    finish styling implementing comparitive button and redirect to gallery.html in index.html

---
+++ script/index.js
@@ -0,0 +1,32 @@
+let inputbox = document.getElementById("inputbox"); 
+let form = document.getElementById("form"); 
+console.log(form);
+
+let textReplace = document.getElementById("textReplace");
+let button = document.getElementById("button");
+console.log(button);
+
+
+
+function inputCompare(event){ 
+    event.preventDefault();
+    console.log("testing"); 
+    console.log(inputbox.value);
+    if(inputbox.value === "one") {
+        textReplace.innerText = "yes";
+
+        setTimeout(() => {
+            window.location.href = "../pages/gallery.html";
+        }, 2000);
+    } else {
+        textReplace.innerText = "no";
+    }
+
+    // textReplace.innerText = inputbox.value === "one" ? "yes" : "no";
+
+
+} 
+
+form.addEventListener("submit", inputCompare);
+
+// setTimeout();
